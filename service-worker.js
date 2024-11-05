4c8d8917f17e3bfbc6ea2b2a645d1b65c7954550
Mauricio Cejas <mauriciocejas91@gmail.com>
2024-11-05 18:13:54 -0300

    Add PWA support with manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'amjest-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles/styles.css',
+    '/img/amjest-logo3.png',
+    // Add any other assets you want to cache
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch assets from cache
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate service worker and delete old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
